6ebc677548f83ffef1d436ef96836d150d1870b4
CodeWithUmair <codewithumair867@gmail.com>
2025-09-01 03:35:41 +0500

    prisma added in the project

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
-@Injectable({
-    
-})
-export class AuthService {}
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+
+    signup() {
+        return { msg: "I'm signed up. 🟩" }
+    }
+
+    signin() {
+        return { msg: "I'm signed in. 🟩" }
+    }
+}
